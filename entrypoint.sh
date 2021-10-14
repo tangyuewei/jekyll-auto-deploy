5d5cc3841126a970530d7ce3ca0fc6a024c65320
tangyuwei <472680811@qq.com>
2021-10-14 09:57:33 +0800

    init commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 DEST="${JEKYLL_DESTINATION:-_site}"
 REPO="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-BRANCH="master"
+BRANCH="gh-pages"
 BUNDLE_BUILD__SASSC=--disable-march-tune-native
 
 echo "Installing gems..."
